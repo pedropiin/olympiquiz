b8c769173dbc87223fa912f8d1c67fb24d3852f5
pedropiin <pedarosa04@gmail.com>
2024-04-19 11:06:59 -0300

    adição de tabela do modo fácil e implementação da função de seleção de atleta com base na dificuldade

--- reactapp/src/select-random-player.tsx
+++ reactapp/src/select-random-player.tsx
@@ -1,8 +1,8 @@
 import * as fs from "fs";
-import * as path from "path";
+import * as path from 'path';
 import { parse } from 'csv-parse';
 
-type Athlete = {
+type Athlete = { 
     id: number;
     name: string;
     sex: string;
@@ -13,6 +13,39 @@ type Athlete = {
     num_medals: number;
 };
 
-const filePath = path.resolve(_dirname, "../../handling_data/data/medalists.csv");
-const headers = ["ID", "NAME", "SEX", "AGE", "COUNTRY", "YEAR", "SPORT", "MEDALS"];
-const fileContent = fs.readFileSync(filePath, {encoding: 'utf-8'});
+const easyMode: boolean = false;
+let filePath;
+if (easyMode) {
+    filePath = path.resolve(__dirname, "../../handling_data/data/medalists-easy.csv");
+} else {
+    filePath = path.resolve(__dirname, "../../handling_data/data/medalists.csv");
+}
+//const headers = ["ID", "NAME", "SEX", "AGE", "COUNTRY", "YEAR", "SPORT", "MEDALS"];
+
+async function selectRandomAthlete(filePath: string): Promise<Athlete | null> {
+    try {
+        const arrayAthletes: Athlete[] = [];
+        const parser = fs
+            .createReadStream(filePath)
+            .pipe(parse({
+            }));
+        
+            for await (const atleta of parser) {
+                arrayAthletes.push(atleta)
+            }
+
+            const randomIndex = Math.floor(Math.random() * arrayAthletes.length);
+            return arrayAthletes[randomIndex];
+    } catch (error) {
+        console.log("Erro no parseamento do arquivo: ", error);
+        return null;
+    }
+}
+
+selectRandomAthlete(filePath).then(randomAthlete => {
+    if (randomAthlete) {
+        console.log(randomAthlete[1]); // nome do atleta aleatório
+    } else {
+        console.log("Deu ruim");
+    }
+})
