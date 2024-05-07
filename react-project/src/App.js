c00c305901de3c5354f7e96ab6177959e229ad71
lipecorradini <luizfelipecorradini@gmail.com>
2024-05-07 16:33:21 -0300

    finalized build

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,9 @@ function App() {
         >
           Olympiquiz!
         </a>
-      </header>
+      </header> */}
+      <h1>Hello! Welcome to Olympiquiz</h1>
+      
     </div>
   );
 }
