7c7d326351abe8d39e87fbc51ed22b42a8138338
pedropiin <pedarosa04@gmail.com>
2024-05-21 17:37:27 -0300

    arrumando typo Home e adicionando form de signup na tela de login

--- react-project/src/pages/Home.js
+++ react-project/src/pages/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
     <div className="home-container">
       <img src={homeImage} alt="Home" className="home-image" />
         <div className='welcome-text'> <p>Welcome to </p></div>
-        <div className='olympiquiz-text'> <p>Olympiquiz</p></div>
+        <div className='olympiquiz-text'> <p>OlympiQuiz</p></div>
         <button className="start-button">Start</button>
         <button className="sign-in-button">Sign in</button>
     
--- react-project/src/pages/Login.js
+++ react-project/src/pages/Login.js
@@ -5,22 +5,41 @@ import '../pages/Login.css';
 //TODO: Inserir símbolo login antes da label do username e senha
 const Login = () => {
     return (
-        <div className='login-container'>
+        <div className='container'>
             <div className='home-text'>
                 <p>OlympiQuiz</p>
             </div>
             <button className='back-button'>Voltar</button>
-            <div className='login-info-container'>
-                <div className='login-text'>
-                    <h1>Login</h1>
+            <div className='form'>
+                <div className='signup-container'>
+                    <div className='signup-text'>
+                        <h1>Sign Up</h1>
+                    </div>
+                    <div className='email-signup-container'>
+                        <label for='email-signup-input'>Email:</label>
+                        <input type='text' id='email-signup-input' name='email-signup-input'></input>
+                    </div>
+                    <div className='username-signup-container'>
+                        <label for='username-signup-input'>Username:</label>
+                        <input type='text' id='username-signup-input' name='username-signup-input'></input>
+                    </div>
+                    <div className='password-signup-container'>
+                        <label for='password-signup-input'>Password:</label>
+                        <input type='password' id='password-signup-input' name='password-signup-input'></input>
+                    </div>
                 </div>
-                <div className='username-container'> 
-                    <label for='username'>Nome de usuário: </label> 
-                    <input type='text' id='username-input' name='username-input' required></input>
-                </div>
-                <div className='password-container'>
-                    <label for='password'>Senha: </label>
-                    <input type='password' id='password-input' name='password-input' required></input>
+                <div className='login-container'>
+                    <div className='login-text'>
+                        <h1>Login</h1>
+                    </div>
+                    <div className='username-login-container'> 
+                        <label for='username-login-input'>Username:</label> 
+                        <input type='text' id='username-login-input' name='username-login-input'></input>
+                    </div>
+                    <div className='password-login-container'>
+                        <label for='password-login-input'>Password:</label>
+                        <input type='password' id='password-login-input' name='password-login-input'></input>
+                    </div>
                 </div>
             </div>
         </div>
