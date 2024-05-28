011835d1e1bf8c5c40741cc5783a7b8360888301
Mariano Cho <mariano.cho@conpec.com.br>
2024-05-28 16:08:16 -0300

    tried to display playing page fail

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Playing from './pages/Playing';
 
 const App = () => {
-  
+
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/Playing/playing" element={<Playing />} />
       </Routes>
     </Router>
   );
--- react-project/src/pages/Playing.js
+++ react-project/src/pages/Playing.js
@@ -1,10 +1,12 @@
 import React from "react";
-import playingImg from '../assets/playingImg.jpg';
+import playingImg from '../assets/playing.jpg';
+import SearchBar from '../components/SearchBar';
 
-function Playing() {
+const Playing = () => {
     return (
         <div className="playing-container">
             <img src={playingImg} alt="Playing" className="playing-image" />
+            <SearchBar />
             <div className="attributes-text"><p>Name            Nationality          Sport          Year        Sex</p></div>
         </div>
     )
