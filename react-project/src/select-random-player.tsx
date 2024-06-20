598d72fe4fd5492da263dba07ad966668440854d
pedropiin <pedarosa04@gmail.com>
2024-06-20 16:02:44 -0300

    beginning of the functionality of the playing page

--- react-project/src/select-random-player.tsx
+++ react-project/src/select-random-player.tsx
@@ -1,8 +1,4 @@
-import * as fs from "fs";
-import * as path from 'path';
-import { parse } from 'csv-parse';
-
-type Athlete = { 
+export type Athlete = { 
     id: number;
     name: string;
     sex: string;
@@ -16,26 +12,20 @@ type Athlete = {
 const easyMode: boolean = false;
 let filePath;
 if (easyMode) {
-    filePath = path.resolve(__dirname, "../../handling_data/data/medalists-easy.csv");
+    filePath = "https://raw.githubusercontent.com/pedropiin/olympiquiz/develop/handling_data/data/medalists-easy.json";
 } else {
-    filePath = path.resolve(__dirname, "../../handling_data/data/medalists.csv");
+    filePath = "https://raw.githubusercontent.com/pedropiin/olympiquiz/develop/handling_data/data/medalists.json";
 }
 //const headers = ["ID", "NAME", "SEX", "AGE", "COUNTRY", "YEAR", "SPORT", "MEDALS"];
 
-async function selectRandomAthlete(filePath: string): Promise<Athlete | null> {
+export async function selectRandomAthlete(filePath: string): Promise<Athlete | null> {
     try {
-        const arrayAthletes: Athlete[] = [];
-        const parser = fs
-            .createReadStream(filePath)
-            .pipe(parse({
-            }));
-        
-            for await (const atleta of parser) {
-                arrayAthletes.push(atleta)
-            }
+        const response = await fetch(filePath);
+        const athletes = await response.json();
 
-            const randomIndex = Math.floor(Math.random() * arrayAthletes.length);
-            return arrayAthletes[randomIndex];
+
+        const randomIndex = Math.floor(Math.random() * athletes.length);
+        return athletes[randomIndex];
     } catch (error) {
         console.log("Erro no parseamento do arquivo: ", error);
         return null;
