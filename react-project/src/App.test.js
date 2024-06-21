0656a80dd17cf473e39458d9d9acb99ee8e8d56b
MrRay0708 <h223911@dac.unicamp.br>
2024-06-21 18:41:15 -0300

    ajustando path

--- react-project/src/App.test.js
+++ react-project/src/App.test.js
@@ -47,36 +47,41 @@ describe ('isPasswordValid', () => {
 
 // teste que verifica a funcionalidade de buscar um nome aleatório, particionamento(saída): nome dentro da lista ou não, valor limite: tamanho da string entre, menor nome para maior nome
 test('should return true if the random athlete belongs to the list and his name lenght is between 5 and 40 characters', async () => {
+  const response = await fetch("https://raw.githubusercontent.com/pedropiin/olympiquiz/develop/handling_data/data/medalists.json");
+  const medalists = await response.json();
   const result = await selectRandomAthlete('../handling_data/data/medalists.csv');
-  expect(result in path.resolve(__dirname, "../../handling_data/data/medalists.csv")).toBe(True); 
+  expect(result in medalists).toBe(True); 
   expect(length(result.name) >= 5 && length(result.name) <= 40)
 });
 
 // teste que verifica a funcionalidade da rotina de buscar os medalistas na lista medalists-easy, particionamento: medalistas e não medalistas e sem números, valor limite: min 11 caracteres, max 19 caracteres
 describe ('isSearchingWorking', () => {
+  const response = fetch("https://raw.githubusercontent.com/pedropiin/olympiquiz/develop/handling_data/data/medalists-easy.json");
+  const medalists_easy = response.json();
+
   test('should return true for a medalist inside the list', async () => {
-    expect(getAthleteInput("Michael Phelps") in path.resolve(__dirname, "../../handling_data/data/medalists-easy.csv")).toBe(True);
+    expect(getAthleteInput("Michael Phelps") in medalists_easy).toBe(True);
   });
   test('should return false for a medalist with numbers on his name', async () => {
-    expect(getAthleteInput("Mich4el Phelps") in path.resolve(__dirname, "../../handling_data/data/medalists-easy.csv")).toBe(False);
+    expect(getAthleteInput("Mich4el Phelps") in medalists_easy).toBe(False);
   });
   test('should return true for a medalist inside the list which name has minimun lenght (11 characters) and no numbers/special characters', async () => {
-    expect(getAthleteInput("Mayra Silva") in path.resolve(__dirname, "../../handling_data/data/medalists-easy.csv")).toBe(True);
+    expect(getAthleteInput("Mayra Silva") in medalists_easy).toBe(True);
   });
   test('should return true for a medalist inside the list which name has maximum lenght (19 characters) and no numbers/special characters', async () => {
-    expect(getAthleteInput("Gilberto Filho Giba") in path.resolve(__dirname, "../../handling_data/data/medalists-easy.csv")).toBe(True);
+    expect(getAthleteInput("Gilberto Filho Giba") in medalists_easy).toBe(True);
   });
   test('should return false for a medalist inside the list which name has less than minimun lenght (11 characters) and no numbers/special characters', async () => {
-    expect(getAthleteInput("Mayra Silv") in path.resolve(__dirname, "../../handling_data/data/medalists-easy.csv")).toBe(False);
+    expect(getAthleteInput("Mayra Silv") in medalists_easy).toBe(False);
   });
   test('should return false for a medalist inside the list which name has more than maximum lenght (19 characters) and no numbers/special characters', async () => {
-    expect(getAthleteInput("Gilberto Filho Gibao") in path.resolve(__dirname, "../../handling_data/data/medalists-easy.csv")).toBe(False);
+    expect(getAthleteInput("Gilberto Filho Gibao") in medalists_easy).toBe(False);
   });
   test('should return false for a name that has special characters', async () => {
-    expect("Simone Biles#" in path.resolve(__dirname, "../../handling_data/data/medalists.csv") in path.resolve(__dirname, "../../handling_data/data/medalists-easy.csv")).toBe(False);
+    expect("Simone Biles#" in path.resolve(__dirname, "../../handling_data/data/medalists.csv") in medalists_easy).toBe(False);
   });
   test('should return false for a name not on the list', async () => {
-    expect("Bruno Cafeo" in path.resolve(__dirname, "../../handling_data/data/medalists.csv") in path.resolve(__dirname, "../../handling_data/data/medalists-easy.csv")).toBe(False);
+    expect("Bruno Cafeo" in path.resolve(__dirname, "../../handling_data/data/medalists.csv") in medalists_easy).toBe(False);
   });
 
 });
