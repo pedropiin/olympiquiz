b208a87cc050bf1358750035e1b7ede378285079
MrRay0708 <h223911@dac.unicamp.br>
2024-06-21 20:28:38 -0300

    alterando o método

---
+++ react-project/src/dataLoader.js
@@ -0,0 +1,21 @@
+// dataLoader.js
+import fs from 'fs';
+import csv from 'csv-parser';
+import path from 'path';
+
+export const loadAthleteNames = (filePath) => {
+  const athletes = [];
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(filePath)
+      .pipe(csv())
+      .on('data', (data) => {
+        athletes.push(data.name);
+      })
+      .on('end', () => {
+        resolve(athletes);
+      })
+      .on('error', (error) => {
+        reject(error);
+      });
+  });
+};
