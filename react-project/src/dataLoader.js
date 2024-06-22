d08e1dc8cf95c36a6a7c28b3f235c9b1739dda2f
MrRay0708 <h223911@dac.unicamp.br>
2024-06-21 22:46:01 -0300

    teste 14

--- react-project/src/dataLoader.js
+++ react-project/src/dataLoader.js
@@ -5,9 +5,7 @@ export async function isNameInDatabase(name, jsonUrl) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
       const data = await response.json();
-      console.log(`Searching for name: ${name}`);
       const nameFound = data.some(athlete => athlete.name === name);
-      console.log(`Found: ${nameFound}`);
       return nameFound;
     } catch (error) {
       console.error("Erro ao buscar ou processar o arquivo JSON:", error);
