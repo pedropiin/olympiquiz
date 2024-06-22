2d9deeb8fa79511dba21f4dc368373054656146a
MrRay0708 <h223911@dac.unicamp.br>
2024-06-21 21:11:58 -0300

    arrumando novo método

--- react-project/src/App.test.js
+++ react-project/src/App.test.js
@@ -5,7 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { passwordChecker } from './passwordChecker';
 import { getAthleteInput } from './playOlimpiquiz';
 import { isNameInDatabase } from './dataLoader';
-import path from 'path';
 
 test('renders Olympiquiz title', () => {
   render(
@@ -50,42 +49,44 @@ describe ('isPasswordValid', () => {
 // teste que verifica a funcionalidade de buscar um nome aleatório, particionamento(saída): nome dentro da lista ou não, valor limite: tamanho da string entre, menor nome para maior nome
 test('should return true if the random athlete belongs to the list and his name length is between 5 and 40 characters', async () => {
   const result = await selectRandomAthlete('../handling_data/data/medalists.csv');
-  const nameFound = await isNameInDatabase(result.name, '../handling_data/data/medalists.csv');
+  const nameFound = await isNameInDatabase(result.name, "../handling_data/data/medalists.csv");
   expect(nameFound).toBe(true); 
+  expect(result.name.length).toBeGreaterThanOrEqual(5);
+  expect(result.name.length).toBeLessThanOrEqual(40);
 });
 
 // teste que verifica a funcionalidade da rotina de buscar os medalistas na lista medalists-easy, particionamento: medalistas e não medalistas e sem números, valor limite: min 11 caracteres, max 19 caracteres
 describe('isSearchingWorking', () => {
   test('should return true for a medalist inside the list', async () => {
-    const nameFound = await isNameInDatabase(getAthleteInput("Michael Phelps"), '../../handling_data/data/medalists-easy.csv');
+    const nameFound = await isNameInDatabase(getAthleteInput("Michael Phelps"), "../../handling_data/data/medalists-easy.csv");
     expect(nameFound).toBe(true);
   });
   test('should return false for a medalist with numbers on his name', async () => {
-    const nameFound = await isNameInDatabase(getAthleteInput("Mich4el Phelps"), '../../handling_data/data/medalists-easy.csv');
+    const nameFound = await isNameInDatabase(getAthleteInput("Mich4el Phelps"), "../../handling_data/data/medalists-easy.csv");
     expect(nameFound).toBe(false);
   });
   test('should return true for a medalist inside the list which name has minimum length (11 characters) and no numbers/special characters', async () => {
-    const nameFound = await isNameInDatabase(getAthleteInput("Mayra Silva"), '../../handling_data/data/medalists-easy.csv');
+    const nameFound = await isNameInDatabase(getAthleteInput("Mayra Silva"), "../../handling_data/data/medalists-easy.csv");
     expect(nameFound).toBe(true);
   });
   test('should return true for a medalist inside the list which name has maximum length (19 characters) and no numbers/special characters', async () => {
-    const nameFound = await isNameInDatabase(getAthleteInput("Gilberto Filho Giba"), '../../handling_data/data/medalists-easy.csv');
+    const nameFound = await isNameInDatabase(getAthleteInput("Gilberto Filho Giba"), "../../handling_data/data/medalists-easy.csv");
     expect(nameFound).toBe(true);
   });
   test('should return false for a medalist inside the list which name has less than minimum length (11 characters) and no numbers/special characters', async () => {
-    const nameFound = await isNameInDatabase(getAthleteInput("Mayra Silv"), '../../handling_data/data/medalists-easy.csv');
+    const nameFound = await isNameInDatabase(getAthleteInput("Mayra Silv"), "../../handling_data/data/medalists-easy.csv");
     expect(nameFound).toBe(false);
   });
   test('should return false for a medalist inside the list which name has more than maximum length (19 characters) and no numbers/special characters', async () => {
-    const nameFound = await isNameInDatabase(getAthleteInput("Gilberto Filho Gibao"), '../../handling_data/data/medalists-easy.csv');
+    const nameFound = await isNameInDatabase(getAthleteInput("Gilberto Filho Gibao"), "../../handling_data/data/medalists-easy.csv");
     expect(nameFound).toBe(false);
   });
   test('should return false for a name that has special characters', async () => {
-    const nameFound = await isNameInDatabase(getAthleteInput("Simone Biles#"), '../../handling_data/data/medalists-easy.csv');
+    const nameFound = await isNameInDatabase(getAthleteInput("Simone Biles#"), "../../handling_data/data/medalists-easy.csv");
     expect(nameFound).toBe(false);
   });
   test('should return false for a name not on the list', async () => {
-    const nameFound = await isNameInDatabase(getAthleteInput("Bruno Cafeo"), '../../handling_data/data/medalists-easy.csv');
+    const nameFound = await isNameInDatabase(getAthleteInput("Bruno Cafeo"), "../../handling_data/data/medalists-easy.csv");
     expect(nameFound).toBe(false);
   });
 });
