78d0e4f6eb298e0ede0023b7f849a984ae7d6125
andresantosr <andresanro03@gmail.com>
2024-06-25 19:32:14 -0300

    adding comparison between players

--- react-project/src/playOlimpiquiz.tsx
+++ react-project/src/playOlimpiquiz.tsx
@@ -2,6 +2,7 @@ import { Athlete } from "./select-random-player.tsx";
 import { selectRandomAthlete } from "./select-random-player.tsx";
 
 let filePath = "https://raw.githubusercontent.com/pedropiin/olympiquiz/develop/handling_data/data/medalists-easy.json";
+let gameAthlete;
 
 export async function getAthleteInput(input) {
     try {
@@ -23,27 +24,35 @@ export async function getAthleteInput(input) {
     }
 }
 
-export async function play(setAthletes) {
+export async function play(input, justStarted) {
     let playerAttempts: Athlete[] = [];
     //let countAttempts = 0;
-    let gameAthlete = await selectRandomAthlete(filePath);
-
+    if(justStarted == 1){
+        gameAthlete = await selectRandomAthlete(filePath);
+    }
+    if (gameAthlete){
+        console.log(gameAthlete.name);
+    }
     if (gameAthlete) {
         // input should be received from integrated function with search bar
         // yet to be implemented. For now, it's hardcoded.
-        let input = "Michael Phelps";
+        console.log(input);
         let inputAthlete = await getAthleteInput(input);
+        if (inputAthlete) {
+            if (inputAthlete.name === gameAthlete.name) { // Player got the right answer
+                console.log("Congratulations!");
 
-        if (inputAthlete.name === gameAthlete.name) { // Player got the right answer
-            console.log("Congratulations!");
-        } else { // Player got the wrong answer
-            //countAttempts++;
-            console.log("Wrong Athlete from input");
-            playerAttempts.push(inputAthlete);
+            } else { // Player got the wrong answer
+                //countAttempts++;
+                console.log("Wrong Athlete from input");
+                playerAttempts.push(inputAthlete);
+                
+            }
         }
     } else {
         console.log("Error while selecting random athlete");
     }
+    return gameAthlete;
 }
 
 
