6cd6ad3bf06cc1d20adc60643fb16eac81ed2dcb
andresantosr <andresanro03@gmail.com>
2024-06-27 00:21:43 -0300

    working

--- react-project/src/playOlimpiquiz.tsx
+++ react-project/src/playOlimpiquiz.tsx
@@ -28,7 +28,7 @@ export async function getAthleteInput(input) {
   }
 }
 
-const Play = ({ initialInput, justStarted }) => {
+export const Play = ({ initialInput, justStarted }) => {
   const [input, setInput] = useState(initialInput);
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [modalMessage, setModalMessage] = useState("");
@@ -106,4 +106,4 @@ const Play = ({ initialInput, justStarted }) => {
   );
 };
 
-export default Play;
+//export default Play;
