ef5c6bffe452a0853dd4ede57753b62f325da022
andresantosr <andresanro03@gmail.com>
2024-06-27 17:07:22 -0300

    modal ajustado

--- react-project/src/pages/Modal.jsx
+++ react-project/src/pages/Modal.jsx
@@ -11,21 +11,28 @@ const Modal = ({ isOpen, onClose, children }) => {
       left: 0,
       width: '100%',
       height: '100%',
-      backgroundColor: 'rgba(0, 0, 0, 0.7)',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'center',
       zIndex: 1050,
     }}>
       <div style={{
-        padding: '20px',
+        padding: '240px',
         background: '#fff',
-        borderRadius: '8px',
-        maxWidth: '500px',
+        borderRadius: '20px',
+        maxWidth: '3200px',
         textAlign: 'center',
+        fontSize: '40px'
       }}>
         {children}
-        <button onClick={onClose} style={{ marginTop: '20px' }}>Close</button>
+        <button onClick={onClose} style={{
+        marginTop: '20px',
+        fontSize: '40px',
+        padding: '10px 20px'
+
+        }}>Close</button>
+        
       </div>
     </div>
   );
